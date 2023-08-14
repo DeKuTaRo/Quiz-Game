68ee3737aa28ba38b8cb88c3bd37593825300a92
DeKuTaRo <thanhtuan09102001@gmail.com>
2023-08-14 22:25:57 +0700

    add some files

---
+++ index.js
@@ -0,0 +1,604 @@
+const questionsBeginner = [
+  {
+    question: "Which is largest animal in the world?",
+    answer: [
+      {
+        text: "Shark",
+        correct: false,
+      },
+      {
+        text: "Blue Whale",
+        correct: true,
+      },
+      {
+        text: "Elephant",
+        correct: false,
+      },
+      {
+        text: "Girraffe",
+        correct: false,
+      },
+    ],
+  },
+  {
+    question: "Which is smallest country in the world?",
+    answer: [
+      {
+        text: "Vatican City",
+        correct: true,
+      },
+      {
+        text: "Bhutan",
+        correct: false,
+      },
+      {
+        text: "Nepal",
+        correct: false,
+      },
+      {
+        text: "Shri Lanka",
+        correct: false,
+      },
+    ],
+  },
+  {
+    question: "Which is largest dessert in the world?",
+    answer: [
+      {
+        text: "Kalahari",
+        correct: false,
+      },
+      {
+        text: "Gobi",
+        correct: false,
+      },
+      {
+        text: "Sahara",
+        correct: false,
+      },
+      {
+        text: "Antarctica",
+        correct: true,
+      },
+    ],
+  },
+  {
+    question: "Which is smallest continent in the world?",
+    answer: [
+      {
+        text: "Asia",
+        correct: false,
+      },
+      {
+        text: "Australia",
+        correct: true,
+      },
+      {
+        text: "Arctic",
+        correct: false,
+      },
+      {
+        text: "Africa",
+        correct: false,
+      },
+    ],
+  },
+];
+
+const questionsImmediate = [
+  {
+    question: "Which one among the following has the largest share of exports from India?",
+    answer: [
+      {
+        text: "Agriculture and Allied Commodity",
+        correct: false,
+      },
+      {
+        text: "Gems and Jewellery",
+        correct: true,
+      },
+      {
+        text: "Handicrafts",
+        correct: false,
+      },
+      {
+        text: "Electronic Goods",
+        correct: false,
+      },
+    ],
+  },
+  {
+    question: "When did the First World War begin?",
+    answer: [
+      {
+        text: "28th July 1914",
+        correct: true,
+      },
+      {
+        text: "20th July 1913",
+        correct: false,
+      },
+      {
+        text: "28th May 1900",
+        correct: false,
+      },
+      {
+        text: "28th June 1914",
+        correct: false,
+      },
+    ],
+  },
+  {
+    question: "Who is known as Man of Blood and Iron?",
+    answer: [
+      {
+        text: "Napoleon",
+        correct: false,
+      },
+      {
+        text: "Bismarck",
+        correct: true,
+      },
+      {
+        text: "Ho Chi Minh",
+        correct: false,
+      },
+      {
+        text: "Sir Walter Scott",
+        correct: false,
+      },
+    ],
+  },
+  {
+    question: "Who was the first woman in space?",
+    answer: [
+      {
+        text: "Sunitha Williams",
+        correct: false,
+      },
+      {
+        text: "Valentina Tereshkova",
+        correct: true,
+      },
+      {
+        text: "Masida namisha",
+        correct: false,
+      },
+      {
+        text: "Tamara Press",
+        correct: false,
+      },
+    ],
+  },
+];
+
+const questionsProffesional = [
+  {
+    question: "Who was the first woman to win a Nobel Prize?",
+    answer: [
+      {
+        text: "Marie Curie",
+        correct: true,
+      },
+      {
+        text: "Mother Teresa",
+        correct: false,
+      },
+      {
+        text: "Margaret Thatcher",
+        correct: false,
+      },
+      {
+        text: "Indira Gandhi",
+        correct: false,
+      },
+    ],
+  },
+  {
+    question: "Which of these countries was not a part of the Soviet Union?",
+    answer: [
+      {
+        text: "Ukraine",
+        correct: false,
+      },
+      {
+        text: "Georgia",
+        correct: false,
+      },
+      {
+        text: "Poland",
+        correct: true,
+      },
+      {
+        text: "Belarus",
+        correct: false,
+      },
+    ],
+  },
+  {
+    question: "Which of these is not one of the Seven Wonders of the Ancient World?",
+    answer: [
+      {
+        text: "The Great Library of Alexandria",
+        correct: true,
+      },
+      {
+        text: "Colossus of Rhodes",
+        correct: false,
+      },
+      {
+        text: "Lighthouse of Alexandria",
+        correct: false,
+      },
+      {
+        text: "Temple of Artemis",
+        correct: false,
+      },
+    ],
+  },
+  {
+    question: 'What is the name of the fairy in the Disney movie "Peter Pan"?',
+    answer: [
+      {
+        text: "Tinker Bell",
+        correct: true,
+      },
+      {
+        text: "Wendy",
+        correct: false,
+      },
+      {
+        text: "Vidia",
+        correct: false,
+      },
+      {
+        text: "Navi",
+        correct: false,
+      },
+    ],
+  },
+  {
+    question: "What is the capital city of Australia?",
+    answer: [
+      {
+        text: "Sydney",
+        correct: false,
+      },
+      {
+        text: "Melbourne",
+        correct: false,
+      },
+      {
+        text: "Canberra",
+        correct: true,
+      },
+      {
+        text: "Brisbane",
+        correct: false,
+      },
+    ],
+  },
+];
+
+const questionsMaster = [
+  {
+    question: "Trong chiến dịch Điện Biên Phủ, binh chủng nào được ưu tiên tấn công đầu tiên?",
+    answer: [
+      {
+        text: "Pháo binh",
+        correct: true,
+      },
+      {
+        text: "Bộ binh",
+        correct: false,
+      },
+      {
+        text: "Thủy binh",
+        correct: false,
+      },
+      {
+        text: "Không quân",
+        correct: false,
+      },
+    ],
+  },
+  {
+    question: "Văn miếu Quốc tử giám - Trường Đại học đầu tiên của Việt Nam được xây dưới triều đại nào?",
+    answer: [
+      {
+        text: "Nguyễn",
+        correct: false,
+      },
+      {
+        text: "Hồ",
+        correct: false,
+      },
+      {
+        text: "Lý",
+        correct: true,
+      },
+      {
+        text: "Lê",
+        correct: false,
+      },
+    ],
+  },
+  {
+    question: "Cuộc khởi nghĩa Lê Lợi kéo dài mấy năm?",
+    answer: [
+      {
+        text: "10 năm",
+        correct: true,
+      },
+      {
+        text: "6 năm",
+        correct: false,
+      },
+      {
+        text: "14 năm",
+        correct: false,
+      },
+      {
+        text: "13 năm",
+        correct: false,
+      },
+    ],
+  },
+  {
+    question: "Ai là vị trạng nguyên trẻ tuổi nhất nước Nam?",
+    answer: [
+      {
+        text: "Nguyễn Hiền",
+        correct: true,
+      },
+      {
+        text: "Nguyễn Bỉnh Khiêm",
+        correct: false,
+      },
+      {
+        text: "Lương Thế Vinh",
+        correct: false,
+      },
+      {
+        text: "Mạc Đĩnh Chi",
+        correct: false,
+      },
+    ],
+  },
+  {
+    question: "Quốc hiệu đầu tiên của nước ta là gì?",
+    answer: [
+      {
+        text: "Âu Lạc",
+        correct: false,
+      },
+      {
+        text: "Văn Lang - Âu Lạc",
+        correct: false,
+      },
+      {
+        text: "Đại Cồ Việt",
+        correct: false,
+      },
+      {
+        text: "Văn Lang",
+        correct: true,
+      },
+    ],
+  },
+  {
+    question: "Quốc hiệu Đại Cồ Việt xuất hiện từ thời vua nào?",
+    answer: [
+      {
+        text: "Ngô Vương (Ngô Quyền)",
+        correct: false,
+      },
+      {
+        text: "Lê Đại Hành (Lê Hoàn)",
+        correct: false,
+      },
+      {
+        text: "Đinh Tiên Hoàng (Đinh Bộ Lĩnh)",
+        correct: true,
+      },
+      {
+        text: " Lý Công Uẩn",
+        correct: false,
+      },
+    ],
+  },
+  {
+    question: "Vị vua nào đã đổi tên nước ta thành Đại Việt?",
+    answer: [
+      {
+        text: "Lê Đại Hành",
+        correct: false,
+      },
+      {
+        text: "Lý Thánh Tông",
+        correct: true,
+      },
+      {
+        text: "Lý Thái Tổ",
+        correct: false,
+      },
+      {
+        text: "Trần Thái Tông",
+        correct: false,
+      },
+    ],
+  },
+  {
+    question: "Ai là người đổi quốc hiệu nước ta thành Đại Ngu?",
+    answer: [
+      {
+        text: "Hồ Quý Ly",
+        correct: true,
+      },
+      {
+        text: "Hồ Nguyên Trừng",
+        correct: false,
+      },
+      {
+        text: "Hồ Hán Thương",
+        correct: false,
+      },
+      {
+        text: "Cả 3 đáp án đều sai",
+        correct: false,
+      },
+    ],
+  },
+  {
+    question: "Triều đại nào dài nhất lịch sử phong kiến Việt Nam?",
+    answer: [
+      {
+        text: "Nhà Hậu Lê",
+        correct: true,
+      },
+      {
+        text: "Nhà Trần",
+        correct: false,
+      },
+      {
+        text: "Nhà Nguyễn",
+        correct: false,
+      },
+      {
+        text: "Nhà Lý",
+        correct: false,
+      },
+    ],
+  },
+];
+
+const questionElement = document.getElementById("question");
+const answerButtons = document.getElementById("answer-buttons");
+const nextButton = document.getElementById("next-btn");
+const playAgainButton = document.getElementById("playAgain-btn");
+const app = document.getElementById("app");
+const levelTitle = document.querySelector(".container h1");
+const level = document.getElementById("level");
+
+let currentLevel;
+var x = document.querySelectorAll(".levelPicker");
+for (var i = 0; i < x.length; i++) {
+  x[i].addEventListener("click", function () {
+    currentLevel = this.value;
+    console.log(currentLevel);
+
+    if (currentLevel == "" || currentLevel == null) {
+      app.style.display = "none";
+    } else if (currentLevel == 1) {
+      level.style.display = "none";
+      levelTitle.style.display = "none";
+      app.style.display = "block";
+      startQuiz(questionsBeginner);
+      handleNextQuestion(questionsBeginner);
+    } else if (currentLevel == 2) {
+      level.style.display = "none";
+      levelTitle.style.display = "none";
+      app.style.display = "block";
+      startQuiz(questionsImmediate);
+      handleNextQuestion(questionsImmediate);
+    } else if (currentLevel == 3) {
+      level.style.display = "none";
+      levelTitle.style.display = "none";
+      app.style.display = "block";
+      startQuiz(questionsProffesional);
+      handleNextQuestion(questionsProffesional);
+    } else if (currentLevel == 4) {
+      level.style.display = "none";
+      levelTitle.style.display = "none";
+      app.style.display = "block";
+      startQuiz(questionsMaster);
+      handleNextQuestion(questionsMaster);
+    }
+  });
+}
+
+let currentQuestionIndex = 0;
+let score = 0;
+
+function startQuiz(questionsData) {
+  currentQuestionIndex = 0;
+  score = 0;
+  nextButton.innerHTML = "Next";
+  showQuestion(questionsData);
+}
+
+function showQuestion(questionsData) {
+  resetState();
+  console.log(questionsData);
+  let currentQuestion = questionsData[currentQuestionIndex];
+  let questionNo = currentQuestionIndex + 1;
+  questionElement.innerHTML = questionNo + ". " + currentQuestion.question;
+
+  currentQuestion.answer.forEach((answer) => {
+    const button = document.createElement("button");
+    button.innerHTML = answer.text;
+    button.classList.add("btn");
+    answerButtons.appendChild(button);
+    if (answer.correct) {
+      button.dataset.correct = answer.correct;
+    }
+    button.addEventListener("click", selectAnswer);
+  });
+}
+
+function resetState() {
+  nextButton.style.display = "none";
+  while (answerButtons.firstChild) {
+    answerButtons.removeChild(answerButtons.firstChild);
+  }
+}
+
+function selectAnswer(e) {
+  const selectedBtn = e.target;
+  const isCorrect = selectedBtn.dataset.correct === "true";
+  if (isCorrect) {
+    selectedBtn.classList.add("correct");
+    score++;
+  } else {
+    selectedBtn.classList.add("incorrect");
+  }
+  Array.from(answerButtons.children).forEach((button) => {
+    if (button.dataset.correct === "true") {
+      button.classList.add("correct");
+    }
+    button.disabled = true;
+  });
+  nextButton.style.display = "block";
+}
+
+function showScore(questionsData) {
+  resetState();
+  questionElement.innerHTML = `You scored ${score} out of ${questionsData.length}!`;
+  nextButton.style.display = "none";
+  playAgainButton.style.display = "block";
+}
+
+function handleNextButton(questionsData) {
+  currentQuestionIndex++;
+  if (currentQuestionIndex < questionsData.length) {
+    showQuestion(questionsData);
+  } else {
+    showScore(questionsData);
+  }
+}
+
+function handleNextQuestion(questionsData) {
+  nextButton.addEventListener("click", () => {
+    if (currentQuestionIndex < questionsData.length) {
+      handleNextButton(questionsData);
+    } else {
+      startQuiz();
+    }
+  });
+}
+
+playAgainButton.addEventListener("click", () => {
+  levelTitle.style.display = "block";
+  level.style.display = "flex";
+  app.style.display = "none";
+});
